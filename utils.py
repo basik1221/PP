7c6100225048813abe3728b28f7b907d5cd31085
basik1221 <264269@student.pwr.edu.pl>
2024-05-18 18:07:34 +0200

    3 commit

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
-# przykladowe funkcje utils .py
+"""przykladowe funkcje utils .py"""
 def add (a: int , b: int) -> int:
+    """sprawdza wynik dodawania"""
     return a + b
 def subtract (a: int , b: int) -> int:
+    """sprawdza wynik odejmowania"""
     return a - b
 def multiply (a: int , b: int) -> int:
+    """sprawdza wynik mnozenia"""
     return a * b
 def divide (a: int , b: int) -> float :
+    """sprawdza wynik dzielenia"""
     return a / b
